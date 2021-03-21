9fa9fd6f6c196b7f251c08f3a0bd0e44c912ba00
Dawoon Choi <dawoon.choi@deliveryhero.co.kr>
2021-03-21 21:11:33 +0900

    feat(background): handle `isAlwaysOn` when suspend

--- src/background.js
+++ src/background.js
@@ -1,18 +1,19 @@
 import isEmpty from 'lodash/isEmpty';
 import keyBy from 'lodash/keyBy';
-import get from 'lodash/get';
 import map from 'lodash/map';
+
+import {
+  storageLocalGetSingle,
+  getTabActivityKey,
+  storageLocalClear,
+  storageLocalGet,
+  storageLocalSet,
+  storageSyncSet,
+} from './storage';
+import { getTabActivity } from './utils';
 
 const color = '#3aa757';
 const defaultSuspendAfter = 30 * 60 * 1000; // 30 mins
-
-async function storageGet(keys) {
-  return new Promise((resolve) => chrome.storage.local.get(keys, resolve));
-}
-
-async function storageGetSingle(key) {
-  return get(await storageGet([key]), key);
-}
 
 function getHashParams(url) {
   return url.split('#')[1]
@@ -28,73 +29,71 @@ async function suspend(tabInfo) {
   const params = `title=${title}&favIconUrl=${favIconUrl}&url=${url}`;
   const suspendedUrl = `chrome-extension://${chrome.runtime.id}/suspended.html#${params}`;
 
-  return chrome.tabs.update(tabInfo.id, { url: suspendedUrl });
+  return console.log(suspendedUrl, 'is now suspended!');
+  // return chrome.tabs.update(tabInfo.id, { url: suspendedUrl });
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  chrome.storage.local.clear(() => console.log('storage cleared!'));
-  chrome.storage.local.set({ suspendAfter: defaultSuspendAfter });
+  storageSyncSet({ color });
+  storageLocalClear().then(() => console.log('storage cleared!'));
+  storageLocalSet({ suspendAfter: defaultSuspendAfter });
 });
 
 chrome.alarms.create({ periodInMinutes: 1.0 });
 
 chrome.alarms.onAlarm.addListener(async () => {
-  const suspendAfter = await storageGetSingle('suspendAfter');
+  const suspendAfter = await storageLocalGetSingle('suspendAfter');
   const inactiveTabs = await chrome.tabs.query({ active: false, url: ['http://*/*', 'https://*/*'] });
   const tabsById = keyBy(inactiveTabs, 'id');
-  const keys = map(inactiveTabs, (tab) => `tabActivity:${tab.id}`);
+  const keys = map(inactiveTabs, (tab) => getTabActivityKey(tab.id));
   console.log('suspendAfter:', suspendAfter);
   console.log('keys:', keys);
 
-  const tabActivities = await storageGet(keys);
+  const tabActivities = await storageLocalGet(keys);
   console.log('tabActivities:', tabActivities);
 
   const updateItems = inactiveTabs.reduce((acc, tab) => {
-    const lastActiveAt = get(tabActivities, `['tabActivity:${tab.id}'].lastActiveAt`);
+    const activity = (tabActivities[getTabActivityKey(tab.id)]
+      || { id: tab.id, lastActiveAt: null, isAlwaysOn: false });
 
-    if (lastActiveAt) {
+    if (activity.lastActiveAt) {
       return acc;
     }
 
-    const item = {
-      id: tab.id,
-      lastActiveAt: Date.now(),
-      isAlwaysOn: false,
-    };
-    return { ...acc, [`tabActivity:${tab.id}`]: item };
+    activity.lastActiveAt = Date.now();
+    return { ...acc, [getTabActivityKey(tab.id)]: activity };
   }, {});
   console.log('updateItems:', updateItems);
 
   // update lastActiveAt in storage
   if (!isEmpty(updateItems)) {
-    await chrome.storage.local.set(updateItems);
+    await storageLocalSet(updateItems);
   }
 
   // suspend tabs
   await Promise.all(map(updateItems, (item) => {
-    if ((Date.now() - item.lastActiveAt) > suspendAfter) {
-      return suspend(tabsById[item.id]);
+    if (item.isAlwaysOn || (Date.now() - item.lastActiveAt) < suspendAfter) {
+      return Promise.resolve();
     }
 
-    return Promise.resolve();
+    return suspend(tabsById[item.id]);
   }));
 });
 
 chrome.tabs.onActivated.addListener(async ({ tabId }) => {
   const suspendedPageUrl = `chrome-extension://${chrome.runtime.id}/suspended.html`;
-  const key = `tabActivity:${tabId}`;
-  const activity = (await storageGetSingle(key))
-    || { id: tabId, lastActiveAt: null, isAlwaysOn: false };
+  const key = getTabActivityKey(tabId);
+  const activity = await getTabActivity(tabId);
   const tab = await chrome.tabs.get(tabId);
 
   activity.lastActiveAt = null;
-  await chrome.storage.local.set({ [key]: activity });
+  await storageLocalSet({ [key]: activity });
 
   console.log('active tab URL:', tab.url);
 
   if (tab.url.startsWith(suspendedPageUrl)) {
     const { url } = getHashParams(tab.url);
-    await chrome.tabs.update(tab.id, { url });
+    console.log(url, 'is now active!');
+    // await chrome.tabs.update(tab.id, { url });
   }
 });
