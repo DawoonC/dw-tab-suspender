308796955ec34c8d4f6f4a78fd60625d124ad9f9
Dawoon Choi <dawoon.choi@deliveryhero.co.kr>
2021-03-28 21:47:14 +0900

    feat(suspended): added tab info & restore button in suspended page

--- src/suspended.js
+++ src/suspended.js
@@ -1,3 +1,15 @@
+import { MDCRipple } from '@material/ripple';
+
+import '../styles/suspended.scss';
+import { restore } from './utils';
+
+const restoreBtn = document.getElementById('restore-btn');
+const tabIcon = document.getElementById('tab-icon');
+const tabTitle = document.getElementById('tab-title');
+const tabUrl = document.getElementById('tab-url');
+// eslint-disable-next-line no-new
+new MDCRipple(restoreBtn);
+
 function setTabTitleAndIcon() {
   const params = window.location.hash
     .slice(1)
@@ -9,6 +21,15 @@ function setTabTitleAndIcon() {
 
   document.querySelector('link[rel="icon"]').href = params.favIconUrl;
   document.title = params.title;
+
+  tabIcon.setAttribute('src', params.favIconUrl);
+  tabTitle.innerText = params.title;
+  tabUrl.innerText = params.url;
 }
 
 setTabTitleAndIcon();
+
+restoreBtn.addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  await restore(tab);
+});
