a5d0bc2315c1f2c2c35f84bdb169951ec7361904
Dawoon Choi <dawoon.choi@deliveryhero.co.kr>
2021-04-02 16:01:03 +0900

    feat(popup): added "Restore all tabs" button in popup

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,20 @@
 import { MDCSwitch } from '@material/switch';
 import { MDCRipple } from '@material/ripple';
 import { MDCList } from '@material/list';
+import map from 'lodash/map';
 
 import '../styles/popup.scss';
 import {
+  getTabActivityKey,
+  storageLocalSet,
+} from './storage';
+import {
   removeFromWhitelist,
   getDomainFromUrl,
+  getTabActivity,
   addToWhitelist,
   getWhitelist,
+  restore,
   suspend,
 } from './utils';
 
@@ -19,6 +26,7 @@ const domainSwitch = document.getElementById('domain-suspend-switch');
 const urlSwitch = document.getElementById('url-suspend-switch');
 const suspendNowLink = document.getElementById('suspend-now-link');
 const settingsLink = document.getElementById('settings-link');
+const restoreAllLink = document.getElementById('restore-all-link');
 
 checkboxList.listElements.map((listItemEl) => new MDCRipple(listItemEl));
 linkList.listElements.map((listItemEl) => new MDCRipple(listItemEl));
@@ -51,6 +59,27 @@ chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
 suspendNowLink.addEventListener('click', async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   await suspend(tab);
+});
+
+restoreAllLink.addEventListener('click', async () => {
+  const suspendedTabs = await chrome.tabs.query({
+    active: false,
+    url: [`chrome-extension://${chrome.runtime.id}/suspended.html*`],
+  });
+
+  // eslint-disable-next-line no-alert
+  if (!window.confirm(`Restore ${suspendedTabs.length} tabs now?`)) {
+    return;
+  }
+
+  await Promise.all(map(suspendedTabs, async (tab) => {
+    const key = getTabActivityKey(tab.id);
+    const activity = await getTabActivity(tab.id);
+
+    activity.lastActiveAt = null;
+    await storageLocalSet({ [key]: activity });
+    await restore(tab);
+  }));
 });
 
 settingsLink.addEventListener('click', () => {
